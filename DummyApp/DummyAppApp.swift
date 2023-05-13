c5a670589f1aa41b81d65498dc071e44cb73e0a6
Russell Yeo <rusyeo@gmail.com>
2023-05-13 10:41:38 +0100

    Initial Commit

---
+++ DummyApp/DummyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DummyAppApp.swift
+//  DummyApp
+//
+//  Created by Russell Yeo on 13/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct DummyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
